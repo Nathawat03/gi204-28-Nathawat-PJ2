a65d868af10cd34d48dfe7eac870ba08e17de3d1
Nathawat03 <Nathawat.sama@bumail.net>
2025-05-01 12:28:51 +0700

    Update Score

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,19 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     public int ScoreCount;
-    public Text ScoreText;
-    void Start()
-    {
-      
-        
-    } // Start
-
+    public TextMeshProUGUI ScoreText;
     
     void Update()
     {
-        ScoreText.text = ScoreCount.ToString();
+        ScoreText.text = ScoreCount.ToString($"Score: {ScoreCount}");
         
     }// Update
     
